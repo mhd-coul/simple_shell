771301f393fba8e3d7eaadaa6fcca561f0776ae3
taky-kongo <abdoulmalickkongo@gmail.com>
2023-05-23 15:09:32 +0000

    Update the file shell.c

--- shell.c
+++ shell.c
@@ -16,18 +16,18 @@
  */
 int tokenize_command(char *command, char **args)
 {
-    int arg_count = 0;
-    char *token;
+	int arg_count = 0;
+	char *token;
 
-    token = strtok(command, " ");
-    while (token != NULL && arg_count < ARGUMENTS_LIMIT)
-    {
-        args[arg_count++] = token;
-        token = strtok(NULL, " ");
-    }
-    args[arg_count] = NULL;
+	token = strtok(command, " ");
+	while (token != NULL && arg_count < ARGUMENTS_LIMIT)
+	{
+		args[arg_count++] = token;
+		token = strtok(NULL, " ");
+	}
+	args[arg_count] = NULL;
 
-    return arg_count;
+	return (arg_count);
 }
 
 /**
@@ -36,72 +36,72 @@ int tokenize_command(char *command, char **args)
  */
 void execute_command(char **args)
 {
-    pid_t pid = fork();
+	pid_t pid = fork();
 
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        wait(NULL);
-    }
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
 }
 
 int main(void)
 {
-    char *command;
-    size_t bufsize = BUFFER_SIZE;
-    ssize_t characters_read;
+	char *command;
+	size_t bufsize = BUFFER_SIZE;
+	ssize_t characters_read;
 
-    command = (char *)malloc(bufsize * sizeof(char));
-    if (command == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+	command = (char *)malloc(bufsize * sizeof(char));
+	if (command == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-    char *args[ARGUMENTS_LIMIT];
-    int arg_count;
+	char *args[ARGUMENTS_LIMIT];
+	int arg_count;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
+	while (1)
+	{
+		printf("#cisfun$ ");
 
-        characters_read = getline(&command, &bufsize, stdin);
-        if (characters_read == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("\n");
-                break;
-            }
-            else
-            {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
+		characters_read = getline(&command, &bufsize, stdin);
+		if (characters_read == -1)
+		{
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("getline");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-        if (command[characters_read - 1] == '\n')
-            command[characters_read - 1] = '\0';
+		if (command[characters_read - 1] == '\n')
+			command[characters_read - 1] = '\0';
 
-        arg_count = tokenize_command(command, args);
+		arg_count = tokenize_command(command, args);
 
-        execute_command(args);
-    }
+		execute_command(args);
+	}
 
-    free(command);
-    return 0;
+	free(command);
+	return (0);
 }
 
